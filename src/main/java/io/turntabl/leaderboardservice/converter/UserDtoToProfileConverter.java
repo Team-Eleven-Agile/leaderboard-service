5544dc321aa920e1c0cd763c9a04d082ba8d2eb5
Phinehas Anteh <85111306+Phiny-sly@users.noreply.github.com>
2022-01-18 17:38:41 +0000

    Update UserDtoToProfileConverter.java
    initial commit

--- src/main/java/io/turntabl/leaderboardservice/converter/UserDtoToProfileConverter.java
+++ src/main/java/io/turntabl/leaderboardservice/converter/UserDtoToProfileConverter.java
@@ -8,7 +8,6 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
