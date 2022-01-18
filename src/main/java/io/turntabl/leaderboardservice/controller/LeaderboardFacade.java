2ebe9e558b33a053e6135de2f6b088aad0d3ead7
Phiny-sly <phinehasanteh2@gmail.com>
2022-01-18 15:21:48 +0000

    Leaderboard sorted according to honours

--- src/main/java/io/turntabl/leaderboardservice/controller/LeaderboardFacade.java
+++ src/main/java/io/turntabl/leaderboardservice/controller/LeaderboardFacade.java
@@ -2,10 +2,12 @@ package io.turntabl.leaderboardservice.controller;
 
 import io.turntabl.leaderboardservice.controller.response.ProfileDto;
 import io.turntabl.leaderboardservice.converter.ProfileToProfileDtoConverter;
+import io.turntabl.leaderboardservice.model.Profile;
 import io.turntabl.leaderboardservice.service.LeaderboardRepositoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -19,6 +21,7 @@ public class LeaderboardFacade {
 
     public List<ProfileDto> getLeaderboard() {
         return leaderboardRepositoryService.getProfiles().stream()
+                .sorted(Comparator.comparing(Profile::getHonour).reversed())
                 .map(profileToProfileDtoConverter::convert)
                 .collect(toList());
     }
@@ -27,6 +30,7 @@ public class LeaderboardFacade {
         return leaderboardRepositoryService.getProfiles().stream()
                 .filter(profile -> profile.getLanguageLevels()
                         .stream().anyMatch(languageLevel -> languageLevel.getName().equals(language)))
+                .sorted(Comparator.comparing(Profile::getHonour).reversed())
                 .map(profileToProfileDtoConverter::convert)
                 .collect(toList());
 
